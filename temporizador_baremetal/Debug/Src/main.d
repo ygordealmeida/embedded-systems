d930c239a09b34d7fba3add0175c99e90ccc6968
ygordealmeida <ygorpereira10@gmail.com>
2024-08-29 20:22:15 -0300

    Temporizador
    Temporizador em baremetal para medir frequências

---
+++ temporizador_baremetal/Debug/Src/main.d
@@ -0,0 +1,17 @@
+Src/main.o: ../Src/main.c \
+ C:/Users/ygorp/STM32Cube/Repository/STM32Cube_FW_F4_V1.28.0/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h \
+ C:/Users/ygorp/STM32Cube/Repository/STM32Cube_FW_F4_V1.28.0/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f446xx.h \
+ C:/Users/ygorp/STM32Cube/Repository/STM32Cube_FW_F4_V1.28.0/Drivers/CMSIS/Include/core_cm4.h \
+ C:/Users/ygorp/STM32Cube/Repository/STM32Cube_FW_F4_V1.28.0/Drivers/CMSIS/Include/cmsis_version.h \
+ C:/Users/ygorp/STM32Cube/Repository/STM32Cube_FW_F4_V1.28.0/Drivers/CMSIS/Include/cmsis_compiler.h \
+ C:/Users/ygorp/STM32Cube/Repository/STM32Cube_FW_F4_V1.28.0/Drivers/CMSIS/Include/cmsis_gcc.h \
+ C:/Users/ygorp/STM32Cube/Repository/STM32Cube_FW_F4_V1.28.0/Drivers/CMSIS/Include/mpu_armv7.h \
+ C:/Users/ygorp/STM32Cube/Repository/STM32Cube_FW_F4_V1.28.0/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h
+C:/Users/ygorp/STM32Cube/Repository/STM32Cube_FW_F4_V1.28.0/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h:
+C:/Users/ygorp/STM32Cube/Repository/STM32Cube_FW_F4_V1.28.0/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f446xx.h:
+C:/Users/ygorp/STM32Cube/Repository/STM32Cube_FW_F4_V1.28.0/Drivers/CMSIS/Include/core_cm4.h:
+C:/Users/ygorp/STM32Cube/Repository/STM32Cube_FW_F4_V1.28.0/Drivers/CMSIS/Include/cmsis_version.h:
+C:/Users/ygorp/STM32Cube/Repository/STM32Cube_FW_F4_V1.28.0/Drivers/CMSIS/Include/cmsis_compiler.h:
+C:/Users/ygorp/STM32Cube/Repository/STM32Cube_FW_F4_V1.28.0/Drivers/CMSIS/Include/cmsis_gcc.h:
+C:/Users/ygorp/STM32Cube/Repository/STM32Cube_FW_F4_V1.28.0/Drivers/CMSIS/Include/mpu_armv7.h:
+C:/Users/ygorp/STM32Cube/Repository/STM32Cube_FW_F4_V1.28.0/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h:
